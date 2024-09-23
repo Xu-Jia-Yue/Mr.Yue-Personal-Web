86a1f711620b2cfdf9acad44f6c00d74906620d9
tiezi <2338699727@qq.com>
2024-09-23 11:25:42 +0800

    code页面的搭建

--- src/pages/Code/index.tsx
+++ src/pages/Code/index.tsx
@@ -1,6 +1,43 @@
 // import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './index.scss'
 
 export default function Code() {
-  return <div className='code'>Code</div>
+  const navigate = useNavigate()
+  return (
+    <div className='code'>
+      <div className='VuePro'>
+        <h2 className='animate__animated animate__flipInX'>Vue Program</h2>
+        <div className='codeContent'>
+          <div className='item'>
+            <img src='/src/images/rabbit.png' alt='' />
+            <span>小兔鲜商城</span>
+          </div>
+          <div className='item'>
+            <img src='/src/images/map.png' alt='' />
+            <span>echart可视化</span>
+          </div>
+        </div>
+      </div>
+      <div className='ReactPro'>
+        <h2 className='animate__animated animate__flipInX'>React Program</h2>
+        <div className='codeContent'>
+          <div className='item'>
+            <img src='/src/images/神秘人.png' alt='' />
+            <span>小兔鲜商城</span>
+          </div>
+          <div className='item'>
+            <img src='/src/images/神秘人.png' alt='' />
+            <span>小兔鲜商城</span>
+          </div>
+        </div>
+      </div>
+      <div className='more'>
+        <p className='view' onClick={() => navigate('/about')}>
+          <span className='allow'> ➜ </span>
+          <span className='text'>View More</span>
+        </p>
+      </div>
+    </div>
+  )
 }
